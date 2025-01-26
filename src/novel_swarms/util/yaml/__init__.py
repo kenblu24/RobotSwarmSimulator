f48d19ddf94f76dbaeb856c8566dc6dceaf8c50f
kenblu24 <kenblu24@hotmail.com>
2025-01-25 21:59:35 -0500

    read yaml and ignore unknown yaml tags

--- src/novel_swarms/util/yaml/__init__.py
+++ src/novel_swarms/util/yaml/__init__.py
@@ -1,7 +1,18 @@
 import yaml
 from .mathexpr import construct_numexpr
 from .include import IncludeLoader, construct_include
+from .unknown import Tagged, construct_undefined, register_undefined
 
 from functools import partial
 
 load = partial(yaml.load, Loader=IncludeLoader)
+
+
+class NaiveLoader(yaml.SafeLoader):
+    pass
+
+
+register_undefined(NaiveLoader)  # this ignores tags with no defined constructor and handles recursion
+# Tagged objects will be wrapped in a Tagged object i.e. isinstance(obj, Tagged) will be True
+# The tag is stored as a string, i.e. obj.tag
+safe_load = partial(yaml.load, Loader=NaiveLoader)
