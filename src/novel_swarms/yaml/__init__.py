2951152685e9eeefdd9d09dc9d255343333c890d
kenblu24 <kenblu24@hotmail.com>
2025-02-03 14:18:21 -0500

    fix pathlib args representation

--- src/novel_swarms/yaml/__init__.py
+++ src/novel_swarms/yaml/__init__.py
@@ -57,14 +57,16 @@ def NDArrayRepresenter(dumper: yaml.Dumper, data: np.ndarray):
     return node
 
 
-def PathRepresenter(dumper, data):
+def PathRepresenter(dumper: yaml.Dumper, data: pathlib.Path):
     tag = "tag:yaml.org,2002:python/object/apply:"
-    function = np.ndarray
+    function = type(data)
     function_name = f"{function.__module__}.{function.__name__}"
-    value = {
-        'resolved': str(data.expanduser().resolve()),
-        'args': (str(data)),
-    }
+    value = {'args': [str(data)],}
+    if isinstance(data, pathlib.Path):
+        try:
+            value['resolved'] = str(data.expanduser().resolve())
+        except (FileNotFoundError, NotADirectoryError, OSError, RuntimeError):
+            value['resolved'] = str(data.expanduser())
     return dumper.represent_mapping(tag + function_name, value)
 
 
