87e4c6de51508cab032b16e5955eca39128cf8b4
Logan <lpr1m3@icloud.com>
2025-03-13 16:16:48 -0400

    Add Keyboard and Controller input human Agent

--- demo/configs/logantest/HumanController.py
+++ demo/configs/logantest/HumanController.py
@@ -13,10 +13,9 @@ class HumanController(AbstractController):
         
         self.control = control
         
-        # pygame.joystick.init()
-        # if pygame.joystick.get_count() > 0:
-        #     self.joystick = pygame.joystick.Joystick(0)
-        #     self.joystick.init()
+        pygame.joystick.init()
+        if pygame.joystick.get_count() > 0:
+            self.joystick = pygame.joystick.Joystick(0)
 
         self.body_color = (255, 0, 255)
         self.v, self.omega = 0, 0
@@ -24,61 +23,38 @@ class HumanController(AbstractController):
 
     def get_actions(self, agent): #uses arrow keys to move
         VEL = 0.3
-        RAD = 0.05
+        RAD_KEY, RAD_CON = 0.1, 1
+        
+        self.handle_controller(VEL, RAD_CON)
+        self.handle_key_press(VEL, RAD_KEY)     
+        
+        return self.v, self.omega
 
-        # if pygame.joystick.get_count() > 0:
-        #     self.joystick = pygame.joystick.Joystick(0)
-        #     self.joystick.init()
 
-        # if self.joystick.get_axis(1) > 0 or self.joystick.get_axis(2) > 0:
-        #     return self.joystick.get_axis(1) * VEL, self.joystick.get_axis(2) * RAD
-        # if pygame.display.get_init():
-        #     event = pygame.event.get()
-        #     # if event.axis == 1:
-        #     #     self.v = event.value * VEL
-        #     # if event.axis == 2:
-        #     #     self.omega = event.value * RAD
-
-        if self.control == "keys-holonomic":
+    def handle_key_press(self, vel, rad):
+        if self.control == "keys-holonomic" and (abs(self.joystick.get_axis(1)) < 0.01 and abs(self.joystick.get_axis(2)) < 0.01 and self.joystick.get_hat(0)[0] == (0, 0)):
             keys = pygame.key.get_pressed()
 
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-                self.omega += RAD
+                self.omega += rad
             if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-                self.omega -= RAD
+                self.omega -= rad
             if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-                self.v = -VEL
+                self.v = -vel
             if keys[pygame.K_UP] or keys[pygame.K_w]:
-                self.v = VEL
+                self.v = vel
             
             if not (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and not (keys[pygame.K_LEFT] or keys[pygame.K_a]):
-                if abs(self.omega) > RAD:
-                    self.omega -= copysign(RAD, self.omega)
-                else:
-                    self.omega = 0
+                self.omega = 0
 
             if not (keys[pygame.K_DOWN] or keys[pygame.K_s]) and not (keys[pygame.K_UP] or keys[pygame.K_w]):
-                # if abs(self.v) > VEL:
-                #     self.v -= copysign(VEL, self.v)
-                # else:
-                #     self.v = 0
                 self.v = 0
 
-        
-        return np.clip(self.v, -0.3, 0.3), np.clip(self.omega, -2, 2)
+        self.v, self.omega = np.clip(self.v, -0.3, 0.3), np.clip(self.omega, -1.5, 1.5)
+            
 
-
-
-
-
-    # def handle_key_press(self, agent, keys):
-    #     if self.control == "keys-holonomic":
-    #         STEP_SIZE = 2
-    #         if keys[pygame.K_RIGHT]:
-    #             self.set_x_pos(self.get_x_pos() + STEP_SIZE)
-    #         if keys[pygame.K_LEFT]:
-    #             self.set_x_pos(self.get_x_pos() - STEP_SIZE)
-    #         if keys[pygame.K_DOWN]:
-    #             self.set_y_pos(self.get_y_pos() + STEP_SIZE)
-    #         if keys[pygame.K_UP]:
-    #             self.set_y_pos(self.get_y_pos() - STEP_SIZE)
+    def handle_controller(self, vel, rad):
+        if pygame.joystick.get_count() > 0 and not (True in pygame.key.get_pressed()):
+            self.v = -self.joystick.get_axis(1) * vel if abs(self.joystick.get_axis(1)) > 0.01 else self.joystick.get_hat(0)[1] * vel
+            self.omega = self.joystick.get_axis(2) * rad if abs(self.joystick.get_axis(2)) > 0.01 else self.joystick.get_hat(0)[0] * rad
+            
