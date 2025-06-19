a9b3aada4b689e78e94e748ca7baaf37f669f88c
Your Name <prabhusoham@outlook.com>
2025-06-19 19:58:41 -0400

    changed the test file

--- demo/configs/teamtest/teamtest.py
+++ demo/configs/teamtest/teamtest.py
@@ -10,11 +10,8 @@ controller = BinaryController((0.4, 0), (0.4, 0.2))
 world_config = RectangularWorldConfig(size=(10, 10), time_step=1 / 40)
 world = RectangularWorld(world_config)
 
-agent1_config = MazeAgentConfig(angle=.0, position=(0, 5), agent_radius=0.1,team="Orange",body_color=(255,165,0))
+agent1_config = MazeAgentConfig(angle=.0, position=(5, 4.2), agent_radius=0.1,team="Green",body_color=(0,255,0))
 agent1 = MazeAgent(agent1_config, world)
-agent1.controller = controller
-sensor = BinaryFOVSensor(agent1,target_team="Blue", theta=0.45, distance=2,)
-agent1.sensors.append(sensor)
 
 world.population.append(agent1)
 
