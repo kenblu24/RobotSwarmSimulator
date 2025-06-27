f446c2f7a294ecfe710783f36e2aa23901447666
jhudd135 <jhudd135@gmail.com>
2025-06-27 14:26:11 -0400

    reduce the number of agents in milling_test.py

--- demo/milling_test.py
+++ demo/milling_test.py
@@ -20,13 +20,13 @@ world = RectangularWorld(world_config)
 # spawned binary controller agent
 controller = StaticController(output=[0.01, 0])
 agent = MazeAgent(MazeAgentConfig(position=(5, 5), agent_radius=0.1, controller=controller), world)
-sensor = BinaryFOVSensor(agent, theta=0.45, distance=2,)
+sensor = BinaryFOVSensor(agent, theta=0.45, distance=2, bias=0)
 agent.sensors.append(sensor)
 controller = BinaryController((0.27, -0.6), (0.27, 0.6))
 agent.controller = controller
 
 # spawner
-spawner = PointAgentSpawner(world, n=25, facing="away", avoid_overlap=True, agent=agent, mode="oneshot")
+spawner = PointAgentSpawner(world, n=10, facing="away", avoid_overlap=True, agent=agent, mode="oneshot")
 world.spawners.append(spawner)
 
 sim(world, start_paused=True)
