d1e7fd63556a9f045f977c3585132a9244f67818
michabay05 <michabay05@gmail.com>
2025-06-30 20:17:26 -0400

    Finished tests pertaining to agent vision (aka. BinaryFOVSensor tests)

--- tests/test_vision.py
+++ tests/test_vision.py
@@ -1,13 +1,21 @@
+import os
+
 from src.swarmsim.sensors.BinaryFOVSensor import BinaryFOVSensor
 from src.swarmsim.world.simulate import main
 from src.swarmsim.world.RectangularWorld import RectangularWorld, RectangularWorldConfig
 from src.swarmsim.agent.Agent import Agent
+from src.swarmsim import yaml as ssyaml
 
 def stop_after_one_step(world: RectangularWorld) -> bool:
     return world.total_steps == 1
 
-def setup_common_world(path: str) -> RectangularWorld:
-    world_config = RectangularWorldConfig.from_yaml(path)
+def load_custom_yaml(path: str) -> tuple[bool, dict]:
+    with open(path, "r") as yf:
+        d = ssyaml.load(yf)
+        return (d["expected"], d["world_setup"])
+
+def setup_common_world(world_setup: dict) -> RectangularWorld:
+    world_config = RectangularWorldConfig(**world_setup)
     world = RectangularWorld(world_config)
     main(world, show_gui=False, stop_detection=stop_after_one_step)
 
@@ -19,39 +27,36 @@ def setup_common_world(path: str) -> RectangularWorld:
 
     return world
 
-def setup_common_agent(world: RectangularWorld) -> Agent:
-    # > Did any agent (w/ name "agent1") see the other agent?
-    agent1 = world.population[0]
-    ## >> Does the first agent have a name of "agent1"?
+def setup_common_agent(world: RectangularWorld) -> BinaryFOVSensor:
+    # > How many agents are there?
+    assert len(world.population) >= 1
+
+    # > Is the first agent the "test-dummy" agent?
+    agent1: Agent = world.population[0]
     assert agent1.name == "agent1"
-    ## >> Does this agent have only one sensor?
+
+    # > Does this agent have a sensor?
     assert len(agent1.sensors) == 1
-
-    return agent1
-
-def test_setup01():
-    world: RectangularWorld = setup_common_world("yaml/setup01.yaml")
-    agent1: Agent = setup_common_agent(world)
-
-    ## >> Is this agent's sensor a BinaryFOVSensor?
     sensor = agent1.sensors[0]
-    assert sensor.as_config_dict()["type"] == "BinaryFOVSensor"
-    ## >> Did the sensor detect anything?
-    bfovsensor: BinaryFOVSensor = agent1.sensors[0]
-    assert bfovsensor.current_state == 1
 
-def test_setup02():
-    world: RectangularWorld = setup_common_world("yaml/setup02.yaml")
-
-    # > Did any agent (w/ name "agent1") see the other agent?
-    agent1 = world.population[0]
-    ## >> Does the first agent have a name of "agent1"?
-    assert agent1.name == "agent1"
-    ## >> Does this agent have only one sensor?
-    assert len(agent1.sensors) == 1
-    ## >> Is that sensor a BinaryFOVSensor
-    sensor = agent1.sensors[0]
+    # > Is this agent's only sensor a BinaryFOVSensor?
     assert sensor.as_config_dict()["type"] == "BinaryFOVSensor"
-    ## >> Did the sensor detect anything?
-    bfovsensor: BinaryFOVSensor = agent1.sensors[0]
-    assert bfovsensor.current_state == 0
+
+    return sensor
+
+def setup_tester(yaml_path: str) -> bool:
+    expected_collided, world_setup = load_custom_yaml(yaml_path)
+    world: RectangularWorld = setup_common_world(world_setup)
+    bfovs: BinaryFOVSensor = setup_common_agent(world)
+
+    collided = bfovs.current_state == 1
+    return collided == expected_collided
+
+def test_setups():
+    dir: str = "vision_setup/"
+
+    for file in os.listdir(dir):
+        if file.endswith(".yaml"):
+            yaml_path: str = os.path.join(dir, file)
+            result = setup_tester(yaml_path)
+            assert result, f"[ERROR] Failed on '{yaml_path}'"
