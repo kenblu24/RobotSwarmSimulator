9d2c72da18bc36f9f810e3e620b17a9d899e4c36
kenblu24 <kenblu24@hotmail.com>
2026-06-16 17:43:21 -0400

    Fix pytest loading blocking tests

--- demo/milling_test.py
+++ demo/milling_test.py
@@ -1,32 +1,24 @@
+import pytest
+
 # from https://kenblu24.github.io/RobotSwarmSimulator/guide/firstrun.html
 
-from swarmsim.world.RectangularWorld import RectangularWorld, RectangularWorldConfig
-from swarmsim.agent.control.StaticController import StaticController
-from swarmsim.world.spawners.DonutSpawner import DonutAgentSpawner
-from swarmsim.world.spawners.AgentSpawner import PointAgentSpawner
-from swarmsim.agent.MazeAgent import MazeAgent, MazeAgentConfig
-from swarmsim.world.simulate import main as sim
-from swarmsim.sensors.BinaryFOVSensor import BinaryFOVSensor
-from swarmsim.agent.control.BinaryController import BinaryController
-from swarmsim.agent.control.HumanController import HumanController
-from swarmsim.agent.control.StaticController import StaticController
-from swarmsim.world.objects.StaticObject import StaticObject, StaticObjectConfig
-from swarmsim.world.objects.Wall import Wall
+def test_milling():
+    from swarmsim import RectangularWorld, RectangularWorldConfig
+    from swarmsim import MazeAgent, MazeAgentConfig
+    from swarmsim.sensors.BinaryFOVSensor import BinaryFOVSensor
+    from swarmsim.agent.control.BinaryController import BinaryController
+    from swarmsim.world.spawners.AgentSpawner import PointAgentSpawner
+    from swarmsim import run_sim
 
-# world
-world_config = RectangularWorldConfig(size=(10, 10), time_step=1 / 40)
-world = RectangularWorld(world_config)
+    world_config = RectangularWorldConfig(size=(10, 10), time_step=1 / 40, stop_at=2)
+    world = RectangularWorld(world_config)
+    agent = MazeAgent(MazeAgentConfig(position=(5, 5), agent_radius=0.1), world)
+    sensor = BinaryFOVSensor(agent, theta=0.45, distance=2,)
+    agent.sensors.append(sensor)
+    controller = BinaryController((0.27, -0.6), (0.27, 0.6), agent)
+    agent.controller = controller
+    spawner = PointAgentSpawner(world, n=6, facing="away", avoid_overlap=True,
+                                agent=agent, mode="oneshot")
+    world.spawners.append(spawner)
 
-# spawned binary controller agent
-controller = StaticController(output=[0.01, 0])
-agent = MazeAgent(MazeAgentConfig(position=(5, 5), agent_radius=0.1, controller=controller), world)
-sensor = BinaryFOVSensor(agent, theta=0.45, distance=2, bias=0)
-agent.sensors.append(sensor)
-controller = BinaryController((0.27, -0.6), (0.27, 0.6))
-agent.controller = controller
-
-# spawner
-spawner = PointAgentSpawner(world, n=10, facing="away", avoid_overlap=True, agent=agent, mode="oneshot")
-world.spawners.append(spawner)
-
-sim(world, start_paused=True)
+    run_sim(world, show_gui=False)
