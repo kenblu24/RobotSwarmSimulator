1922a2a5cd06183d46c1cbb18f5c1fa1a2a9999a
kenblu24 <kenblu24@hotmail.com>
2026-06-17 08:27:33 -0400

    test for circliness

--- demo/milling_test.py
+++ demo/milling_test.py
@@ -8,9 +8,10 @@ def test_milling():
     from swarmsim.sensors.BinaryFOVSensor import BinaryFOVSensor
     from swarmsim.agent.control.BinaryController import BinaryController
     from swarmsim.world.spawners.AgentSpawner import PointAgentSpawner
+    from swarmsim.metrics import Circliness
     from swarmsim import run_sim
 
-    world_config = RectangularWorldConfig(size=(10, 10), time_step=1 / 40, stop_at=2)
+    world_config = RectangularWorldConfig(size=(10, 10), time_step=1 / 40, stop_at=600)
     world = RectangularWorld(world_config)
     agent = MazeAgent(MazeAgentConfig(position=(5, 5), agent_radius=0.1), world)
     sensor = BinaryFOVSensor(agent, theta=0.45, distance=2,)
@@ -20,5 +21,9 @@ def test_milling():
     spawner = PointAgentSpawner(world, n=6, facing="away", avoid_overlap=True,
                                 agent=agent, mode="oneshot")
     world.spawners.append(spawner)
+    world.metrics.append(Circliness(avg_history_max=100))
+    world.metrics[0].attach_world(world)
 
     run_sim(world, show_gui=False)
+
+    assert world.metrics[0].out_current()[1] > 0.5
