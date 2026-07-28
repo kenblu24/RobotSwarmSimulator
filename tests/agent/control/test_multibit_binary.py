26eeade7b6b11816552202de674e86b8e5668467
michabay05 <michabay05@gmail.com>
2026-07-28 13:21:02 -0400

    fix: use `get_actions()` instead of `outputs`

--- tests/agent/control/test_multibit_binary.py
+++ tests/agent/control/test_multibit_binary.py
@@ -1,13 +1,25 @@
-import swarmsim
+import unittest.mock as mock
 
-
+from swarmsim.sensors.BinaryFOVSensor import BinaryFOVSensor
 from swarmsim.agent.control.MultibitBinaryController import MultiBinaryController
 from swarmsim.agent.MazeAgent import MazeAgent
-from swarmsim.sensors.BinaryFOVSensor import BinaryFOVSensor
 
 
-def test_init():
-    agent = MazeAgent()
+def check_mbc_agent(controller: MultiBinaryController, sens_values: list[bool], expected: tuple[float, float]):
+    agent = mock.MagicMock(spec=MazeAgent)
+
+    # Ensure controller sensor id list matches length of sensor values
+    assert len(sens_values) == len(controller.sensor_ids)
+    agent.sensors = [mock.MagicMock(spec=BinaryFOVSensor) for _ in sens_values]
+    for i, sv in enumerate(sens_values):
+        agent.sensors[i].current_state = sv
+
+    assert isinstance(controller, MultiBinaryController)
+    agent.controller = controller
+    assert controller.get_actions(agent) == expected
+
+
+def test_mbc_01():
     controller = MultiBinaryController(
         outputs={
             0b00: (0.0, 0.0),
@@ -18,22 +30,28 @@ def test_init():
         default_output=(0.5, 0.5),
         sensor_ids=[0, 1]
     )
-    agent.controller = controller
-    controller.set_agent(agent)
-    agent.sensors.append(BinaryFOVSensor(agent, 0, fov=1.0))
-    agent.sensors.append(BinaryFOVSensor(agent, 1, fov=1.0))
-    assert len(agent.sensors) == 2
-    assert agent.sensors[0].current_state == 0
-    assert agent.sensors[1].current_state == 0
-    assert agent.controller.get_actions(agent) == (0.5, 0.5)
 
-    # test with default output
+    check_mbc_agent(controller, [False, False], (0.0, 0.0))
+    check_mbc_agent(controller, [True , False], (0.0, 1.0))
+    check_mbc_agent(controller, [False, True ], (1.0, 0.0))
+    check_mbc_agent(controller, [True , True ], (1.0, 1.0))
+
+
+def test_mbc_02():
     controller = MultiBinaryController(
         outputs={
             0b00: (0.0, 0.0),
             0b01: (0.0, 1.0),
             0b10: (1.0, 0.0),
-            0b11: (1.0, 1.0),
+            # Intentionally skipping this value
+            # 0b11: (1.0, 1.0),
         },
+        default_output=(0.5, 0.5),
         sensor_ids=[0, 1]
     )
+
+    check_mbc_agent(controller, [False, False], (0.0, 0.0))
+    check_mbc_agent(controller, [True , False], (0.0, 1.0))
+    check_mbc_agent(controller, [False, True ], (1.0, 0.0))
+    check_mbc_agent(controller, [True , True ], (0.5, 0.5))
+
