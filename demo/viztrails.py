8129d172a4b513e0192f1c25ab47a18248ab53eb
michabay05 <michabay05@gmail.com>
2026-08-22 14:59:42 -0400

    feat: draw each trail on different surfaces to allow better blending

--- demo/viztrails.py
+++ demo/viztrails.py
@@ -9,43 +9,48 @@ from swarmsim.world.simulate import main
 
 
 class VizTrail:
-    def __init__(self, interval=2, window=100):
+    def __init__(self, interval: int = 2, window: int | None = None, opacity=0.5):
+        assert 0. <= opacity <= 1., "Opacity should be in [0, 1]"
+        
         self.agent_pos: dict[str, list[NDArray]] = {}
-        self.agent_cfg: dict = {}
+        self.agent_cfg: dict[str, tuple[float, pygame.Surface]] = {}
 
         self.timesteps = 0
         self.interval = interval
         self.window = window
-        self.alpha_falloff = lambda x: 0.85
-        # self.alpha_falloff = lambda x: 0.9 * x/self.window
+        self.opacity = opacity
 
-    def _update_positions(self, world: RectangularWorld):
+    def _update_positions(self, world: RectangularWorld, screen_size):
         for agent in world.population:
             if agent.name not in self.agent_pos:
                 self.agent_pos[agent.name] = []
-                self.agent_cfg[agent.name] = agent.radius
+                self.agent_cfg[agent.name] = agent.radius, pygame.Surface(screen_size, pygame.SRCALPHA)
 
             self.agent_pos[agent.name].append((agent.getPosition().copy(), agent.angle))
 
-
     def draw(self, screen, world: RectangularWorld):
-        def color_hsla(color: pygame.Color, index: int, angle_rad: float, s=0.5, l=0.5):
-            color.hsla = np.rad2deg(angle_rad)%360., s*100., l*100., self.alpha_falloff(index)*100.
+        def color_hsla(color: pygame.Color, angle_rad: float, s=0.5, l=0.5):
+            color.hsla = np.rad2deg(angle_rad)%360., s*100., l*100., self.opacity*100.
 
         if self.timesteps % self.interval == 0:
-            self._update_positions(world)
+            self._update_positions(world, screen.get_size())
 
         pan, zoom = world.pos, world.zoom
         surface = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
         color = pygame.Color("white")
         for name, orientation in self.agent_pos.items():
-            radius = self.agent_cfg[name]
-            for i, (pos, heading) in enumerate(orientation[-self.window:]):
-                color_hsla(color, i, heading)
+            radius, surface = self.agent_cfg[name]
+            surface.fill("#00000000")
+            if self.window is not None:
+                orientation = orientation[-self.window:]
+
+            for (pos, heading) in orientation:
+                color_hsla(color, heading)
                 pygame.draw.circle(surface, color, pos * zoom, radius * zoom)
 
         self.timesteps += 1
-        screen.blit(surface, pan)
+        for _, surface in self.agent_cfg.values():
+            screen.blit(surface, pan)
 
 
 class VizTest(DifferentialDriveGUI):
@@ -54,8 +59,8 @@ class VizTest(DifferentialDriveGUI):
         self.tracker = VizTrail()
 
     def draw(self, screen):
-        super().draw(screen)
         self.tracker.draw(screen, self.world)
+        super().draw(screen)
 
 
 world_cfg = config_from_yaml("demo/configs/turbopi-milling/world.yaml")
