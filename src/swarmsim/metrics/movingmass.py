1585387882d23cb6b451e594960359d9a39a8bae
michabay05 <michabay05@gmail.com>
2026-09-10 22:26:33 -0400

    fix: missing center of mass

--- src/swarmsim/metrics/movingmass.py
+++ src/swarmsim/metrics/movingmass.py
@@ -11,10 +11,10 @@ class MovingMass(Aggregation):
 
     def center_of_mass(self):
         # NOTE(mabay): Copied this over from 'RadialVarianceMetric'
-        positions = np.asarray([agent.getPosition() for agent in self.population])
+        positions = np.asarray([agent.getPosition() for agent in self.parent.population])
         return positions.mean(axis=0)
 
     def _calculate(self):
         centroid = self.center_of_mass()
         self.centroids.append(centroid)
-        return self.agg._calculate() + np.linalg.norm(centroid - self.centroids[0])
+        return super()._calculate() + np.linalg.norm(centroid - self.centroids[0])
