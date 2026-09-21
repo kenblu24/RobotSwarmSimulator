d46be406aaaf31918ca55d5ffc01913ac8677d86
michabay05 <michabay05@gmail.com>
2026-09-20 23:46:45 -0400

    feat: changed moving mass to use for delta per step

--- src/swarmsim/metrics/movingmass.py
+++ src/swarmsim/metrics/movingmass.py
@@ -1,3 +1,4 @@
+import pygame
 import numpy as np
 
 from .aggregation import Aggregation
@@ -9,15 +10,39 @@ class MovingMass(Aggregation):
 
         self.centroids = []
         self.weights = weights
+        self._invert_multiplier = 1
+
+    @property
+    def positions(self):
+        return np.asarray([agent.getPosition() for agent in self.parent.population])
+
+    @property
+    def agg_value(self):
+        return 1. - super()._calculate()
 
     def center_of_mass(self):
         # NOTE(mabay): Copied this over from 'RadialVarianceMetric'
-        positions = np.asarray([agent.getPosition() for agent in self.parent.population])
-        return positions.mean(axis=0)
+        return self.positions.mean(axis=0)
 
     def _calculate(self):
         agg_wt, disp_wt = self.weights
         centroid = self.center_of_mass()
-        self.centroids.append(centroid)
-        disp_value = np.linalg.norm(centroid - self.centroids[0])
-        return agg_wt * super()._calculate() + disp_wt * np.clip(disp_value, 0, 1)
+        if len(self.centroids) == 0:
+            # NOTE(mabay): Insert centroid twice, at first, because I need two values to start
+            # calculating
+            self.centroids.append(centroid)
+            self.centroids.append(centroid)
+        else:
+            self.centroids.append(centroid)
+
+        disp_value = np.linalg.norm(self.centroids[-1] - self.centroids[-2])
+        return agg_wt * self.agg_value + disp_wt * disp_value
+
+    def draw(self, screen, zoom=1.0):
+        pan, zoom = self.world.pos, self.world.zoom
+        last_centroid, last_last_centroid = self.centroids[-1], self.centroids[-2]
+        pygame.draw.line(screen, "#00ffff", last_centroid * zoom + pan, last_last_centroid * zoom + pan, width=2)
+        pygame.draw.circle(screen, "#ffff00", last_centroid * zoom + pan, 0.05 * zoom)
+        pygame.draw.circle(screen, "#ffff00", last_last_centroid * zoom + pan, 0.05 * zoom)
+
+        
